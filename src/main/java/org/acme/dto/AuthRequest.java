b2d741050081ff67db35b7362f0f2eabf9a690ce
Gustavo <gustavo.souza@accurate.com.br>
2021-07-22 00:16:46 -0300

    Feature: Auth resourcesand DTOs

---
+++ src/main/java/org/acme/dto/AuthRequest.java
@@ -0,0 +1,14 @@
+package org.acme.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class  AuthRequest {
+
+    public String username;
+    public String password;
+}
